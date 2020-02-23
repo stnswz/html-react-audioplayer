63a12107c677b7eb0070720d589f264528c40c59
Steffen Schwarz <stn_swz@gmx.net>
2020-02-23 21:10:26 +0100

    Adds audio event function

--- src/utils/PlayControl.ts
+++ src/utils/PlayControl.ts
@@ -1,6 +1,6 @@
 /**
  * PlayControl is a singleton class thats containing the HTMLAudioElement for
- * playing and controling the audio within the player. PlayControl can be importet 
+ * playing and controling the audio within the player. PlayControl can get imported 
  * everywhere to get access to it. It also has access to the redux store, for
  * getting neccessary data from the store and for dispatching events.
  */
@@ -14,6 +14,7 @@ export class PlayControl {
 
     private constructor() {
         this.audio = new Audio();
+        this.audio.onended = this.audioOnEndedEvent;
         this.intervalId = 0;
     }
 
@@ -26,6 +27,12 @@ export class PlayControl {
 
     public setStore( store:any ) {
         this.store = store;
+    }
+
+    private audioOnEndedEvent(ev:any) {
+        //const curTrackIndex = this.playControlData.trackIndex;
+        //this.trackIndexChanged( curTrackIndex+1 );
+        //this.props.onTrackSkipped( this.playControlData.trackIndex );
     }
 
     /*public getStore():any {
