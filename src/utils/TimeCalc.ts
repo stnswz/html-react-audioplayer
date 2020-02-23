7a2aeb91bab0333be24421b4ddaf0b877b910e86
Steffen Schwarz <stn_swz@gmx.net>
2020-02-23 19:11:46 +0100

    Adds comments

--- src/utils/TimeCalc.ts
+++ src/utils/TimeCalc.ts
@@ -1,10 +1,20 @@
+import { ITrack } from "../app/definitions/ITrack";
+
 export class TimeCalc {
 
-    public static getDuration():string {
+    /**
+     * Translates the given duration from a number to a formatted string.
+     * @param duration 
+     */
+    public static getDuration( duration:number ):string {
         return "00:00";
     }
 
-    public static getCalculatedFulltime():string {
+    /**
+     * Calculates the total time of all tracks together in the given track list.
+     * @param tracks 
+     */
+    public static getCalculatedFulltime( tracks:Array<ITrack>):string {
         return "00:00";
     }
 }
