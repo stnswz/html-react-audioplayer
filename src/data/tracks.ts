a0de7e64042002eff48434e33bcb66f0c79dc245
Steffen Schwarz <stn_swz@gmx.net>
2020-02-24 02:03:52 +0100

    Updates tracks data

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -1,11 +1,11 @@
 import {ITrack} from "./../app/definitions/ITrack"
 
 const tracks:Array<ITrack> = [ 
-    {id:0, interpret: "Nuyorican Soul", title: "Pienso En Ti (Nuyorican Mix)", duration: 60, image:"", src:"./audio/Nuyorican_Soul-Pienso_En_Ti.mp3"},
-    {id:1, interpret: "Crossroads feat Teisha Marie", title: "Ain't Nobody Like You (Soulpersona Remix)", duration: 60, image:"", src:"./audio/Crossroads_Aint-Nobody-Like-You.mp3"},
-    {id:2, interpret: "Siri Svegler", title: "One Step Forward", duration: 60, image:"", src:"./audio/Siri-Svegler_One-Step-Forward.mp3"},
-    {id:3, interpret: "Nat King Cole", title: "Lush Life (produced by Cee Lo Green)", duration: 60, image:"", src:"./audio/Nat-King-Cole_Lush-Life.mp3"},
-    {id:4, interpret: "Marbert Rocel", title: "Hush Now", duration: 60, image:"", src:"./audio/Marbert-Rocel_Hush-Now.mp3"},
+    {id:0, interpret: "Nuyorican Soul", title: "Pienso En Ti (Nuyorican Mix)", duration: 387, image:"", src:"./audio/Nuyorican_Soul-Pienso_En_Ti.mp3"},
+    {id:1, interpret: "Crossroads feat Teisha Marie", title: "Ain't Nobody Like You (Soulpersona Remix)", duration: 316, image:"", src:"./audio/Crossroads_Aint-Nobody-Like-You.mp3"},
+    {id:2, interpret: "Siri Svegler", title: "One Step Forward", duration: 230, image:"", src:"./audio/Siri-Svegler_One-Step-Forward.mp3"},
+    {id:3, interpret: "Nat King Cole", title: "Lush Life (produced by Cee Lo Green)", duration: 196, image:"", src:"./audio/Nat-King-Cole_Lush-Life.mp3"},
+    {id:4, interpret: "Marbert Rocel", title: "Hush Now", duration: 334, image:"", src:"./audio/Marbert-Rocel_Hush-Now.mp3"},
     {id:5, interpret: "Marsmobil", title: "Patience", duration: 60, image:"", src:"./audio/Marsmobil_Patience.mp3"},
     {id:6, interpret: "Jazzanova", title: "Theme From Belle Et Fou", duration: 60, image:"", src:"./audio/Jazzanova_Theme-From-Belle-Et-Fou.mp3"},
 ]
