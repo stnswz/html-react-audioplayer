12d7cdbc2518b12c09bff699244a0104fa1f0636
Steffen Schwarz <stn_swz@gmx.net>
2020-02-24 21:32:16 +0100

    Simplifies code

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,7 @@ import Player from "./components/Player";
 import Playlist from "./components/Playlist";
 import './css/App.css';
 
-interface IState {
-    /** empty */
-}
-interface IProps {
-    /** empty */
-}
-
-class App extends Component<IProps, IState>  {
-
-    constructor(props:IProps) { 
-        super(props);
-        this.state = {
-            height: 0,
-        }
-    }
+class App extends Component {
 
     public render(): ReactElement {
 
