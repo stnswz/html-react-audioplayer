73d26d99b7f125f6a66a158af618ed31ad106de7
Steffen Schwarz <stn_swz@gmx.net>
2020-02-24 02:03:01 +0100

    Adds new action types

--- src/redux/actions/types/actionTypes.ts
+++ src/redux/actions/types/actionTypes.ts
@@ -1,2 +1,4 @@
 export const SET_PLAYING:string     = "SET_PLAYING";
 export const SET_TRACKINDEX:string  = "SET_TRACKINDEX";
+export const SET_PLAYTIME_UPDATE:string  = "SET_PLAYTIME_UPDATE";
+export const SET_PLAYING_HAS_FINISHED:string  = "SET_PLAYING_HAS_FINISHED";
