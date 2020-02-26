e0489bd07e30b379fe9ab33cef43b0c87a59bac7
Steffen Schwarz <stn_swz@gmx.net>
2020-02-26 21:39:40 +0100

    Adds new type for volume control

--- src/redux/actions/types/actionTypes.ts
+++ src/redux/actions/types/actionTypes.ts
@@ -1,4 +1,5 @@
-export const SET_PLAYING:string     = "SET_PLAYING";
+export const SET_VOLUME:string  = "SET_VOLUME";
+export const SET_PLAYING:string  = "SET_PLAYING";
 export const SET_TRACKINDEX:string  = "SET_TRACKINDEX";
 export const SET_PLAYTIME_UPDATE:string  = "SET_PLAYTIME_UPDATE";
 export const SET_PLAYING_HAS_FINISHED:string  = "SET_PLAYING_HAS_FINISHED";
