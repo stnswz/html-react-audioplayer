2fadf4b31a038e8e0d10dcb8ee83f2ee2e77f483
Steffen Schwarz <stn_swz@gmx.net>
2020-03-14 14:02:52 +0100

    Fixes volume bug (when switching track)

--- src/redux/reducers/playReducer.ts
+++ src/redux/reducers/playReducer.ts
@@ -18,10 +18,10 @@ const reducer = (state:IPlayState = initialState, action:any) => {
         case types.SET_PLAYING:
             return { ...state, playStatus: action.payload.playStatus }
         case types.SET_TRACKINDEX:
-            return { ...initialState, trackIndex: action.payload.trackIndex, selectedTrack:action.payload.selectedTrack }
+            return { ...initialState, trackIndex: action.payload.trackIndex, selectedTrack:action.payload.selectedTrack, volume: state.volume }
         case types.SET_PLAYTIME_UPDATE:
             return { ...state, playedTime: action.payload.playedTime, playedPercent:action.payload.playedPercent }
-        case types.SET_VOLUME:
+        case types.SET_VOLUME: 
             return { ...state, volume: action.payload.volume }
         case types.SET_PLAYING_HAS_FINISHED:
             return { ...state, playStatus: playStatus.STOPPED, playedTime:0, playedPercent:0 }
